51688be8c70c341d5ba7108af4d5d7f8cadd43bd
David Denton <denton.david@gmail.com>
2017-09-21 16:39:10 +0100

    First stab at Record Traffic. API might need a little work

--- http4k-core/src/main/kotlin/org/http4k/filter/RecordTraffic.kt
+++ http4k-core/src/main/kotlin/org/http4k/filter/RecordTraffic.kt
@@ -5,78 +5,84 @@ import org.http4k.core.HttpMessage
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.parse
+import org.http4k.core.then
+import org.http4k.filter.TrafficStore.RecordMode
 import java.io.File
 import java.util.*
 
-interface TrafficStorage {
-    operator fun get(req: Request): Response?
-    operator fun set(req: Request, resp: Response)
+object TrafficCache {
+    object Disk {
+        operator fun invoke(baseDir: String = ".") = Filter { next ->
+            {
+                val file = File(it.toFolder(baseDir), "response.txt")
+                if (file.exists()) Response.parse(String(file.readBytes())) else next(it)
+            }
+        }
+    }
 
-    companion object {
+    object Memory {
+        operator fun invoke(cache: Map<Request, Response>) = Filter { next ->
+            {
+                cache[it] ?: next(it)
+            }
+        }
+    }
+}
 
-        /**
-         * Write selected traffic in a tree to the passed base folder (using base64 encoded requests as a key). Repeated traffic is returned
-         * from the stored version. DOES NOT PROVIDE ANY INVALIDATION OR OTHER CACHE-CONTROL SEMANTICS.
-         */
-        object Disk {
-            operator fun invoke(baseDir: String = ".", mode: RecordMode = RecordMode.All): TrafficStorage =
-                object : TrafficStorage {
-                    override fun get(req: Request): Response? {
-                        val file = File(req.toFolder(baseDir), "response.txt")
-                        return if (file.exists()) Response.parse(String(file.readBytes())) else null
-                    }
+object TrafficStore {
 
-                    override fun set(req: Request, resp: Response) {
-                        val requestFolder = req.toFolder(baseDir)
-                        requestFolder.mkdirs()
-                        mode.store(File(requestFolder, "request.txt"), req)
-                        mode.store(File(requestFolder, "response.txt"), resp)
-                    }
-                }
+    // replace with predicate...
+    enum class RecordMode(private vararg val record: RecordMode) {
+        None(), RequestOnly(), ResponseOnly(), All(RequestOnly, ResponseOnly);
 
-            enum class RecordMode(private vararg val record: RecordMode) {
-                None(), RequestOnly(), ResponseOnly(), All(RequestOnly, ResponseOnly);
+        fun store(file: File, r: org.http4k.core.Request) {
+            if (record.contains(RequestOnly)) r.writeTo(file)
+        }
 
-                fun store(file: File, r: org.http4k.core.Request) {
-                    if (record.contains(RequestOnly)) r.writeTo(file)
-                }
+        fun store(file: File, r: Response) {
+            if (record.contains(ResponseOnly)) r.writeTo(file)
+        }
 
-                fun store(file: File, r: org.http4k.core.Response) {
-                    if (record.contains(ResponseOnly)) r.writeTo(file)
-                }
+        private fun HttpMessage.writeTo(file: File) {
+            file.createNewFile()
+            file.writeBytes(toString().toByteArray())
+        }
+    }
 
-                private fun HttpMessage.writeTo(file: File) {
-                    file.createNewFile()
-                    file.writeBytes(toString().toByteArray())
+    object Disk {
+        operator fun invoke(baseDir: String = ".", mode: RecordMode = RecordMode.All) = Filter { next ->
+            {
+                val requestFolder = it.toFolder(baseDir)
+                requestFolder.mkdirs()
+                mode.store(File(requestFolder, "request.txt"), it)
+                next(it).apply {
+                    mode.store(File(requestFolder, "response.txt"), this)
                 }
             }
         }
+    }
 
-        /**
-         * Provide a simple in-memory store (using base64 encoded requests as a key). Useful for Testing, but
-         * DOES NOT PROVIDE ANY INVALIDATION OR OTHER CACHE-CONTROL SEMANTICS.
-         */
-        fun Memory(): TrafficStorage {
-            val cache = linkedMapOf<String, org.http4k.core.Response>()
-            return object : TrafficStorage {
-                override fun get(req: Request) = cache[req.identify()]
-
-                override fun set(req: Request, resp: Response) {
-                    cache[req.identify()] = resp
+    object Memory {
+        operator fun invoke(cache: MutableMap<Request, Response>) = Filter { next ->
+            {
+                next(it).apply {
+                    cache[it] = this
                 }
             }
         }
+    }
+}
+
+object TrafficCachingProxy {
+    fun Disk(baseDir: String = ".", mode: RecordMode = RecordMode.All): Filter =
+        TrafficCache.Disk(baseDir).then(TrafficStore.Disk(baseDir, mode))
+
+    fun Memory(): Filter {
+        val cache = linkedMapOf<Request, Response>()
+        return TrafficCache.Memory(cache).then(TrafficStore.Memory(cache))
     }
 }
 
 private fun Request.toFolder(baseDir: String): File = File(File(if (baseDir.isEmpty()) "." else baseDir, uri.path), this.identify())
 
 private fun HttpMessage.identify() = String(Base64.getEncoder().encode(toString().toByteArray()))
-
-object RecordTraffic {
-    fun to(store: TrafficStorage) = Filter { next ->
-        {
-            store[it]?.let { it } ?: next(it).apply { store[it] = this }
-        }
-    }
-}
--- http4k-core/src/test/kotlin/org/http4k/filter/RecordTrafficTest.kt
+++ http4k-core/src/test/kotlin/org/http4k/filter/RecordTrafficTest.kt
@@ -6,18 +6,18 @@ import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status
 import org.http4k.core.then
-import org.http4k.filter.TrafficStorage.Companion
 
 
 fun main(args: Array<String>) {
     val next: HttpHandler = {
         Response(Status.OK).body("hello")
     }
-    val disk = RecordTraffic.to(Companion.Disk()).then(next)
+
+    val disk = TrafficCachingProxy.Disk().then(next)
+
+    val memory = TrafficCachingProxy.Memory().then(next)
 
     val request = Request(Method.GET, "/asdasd/foo?asdas=asdasd").body("hello").header("asdas", "asdsad")
-
-    val memory = RecordTraffic.to(TrafficStorage.Memory()).then(next)
     println(disk(request))
     println(disk(request))
 
