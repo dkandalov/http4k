de2cea4d7d2e7c680d3b77a626aa3dafc4f5ee73
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2024-04-26 16:37:12 +0100

    tidy up OpenApi3KondorTest

--- http4k-contract/openapi/src/test/kotlin/org/http4k/contract/openapi/v3/OpenApi3KondorTest.kt
+++ http4k-contract/openapi/src/test/kotlin/org/http4k/contract/openapi/v3/OpenApi3KondorTest.kt
@@ -8,50 +8,23 @@ import com.ubertob.kondor.json.obj
 import com.ubertob.kondor.json.str
 import org.http4k.contract.ContractRendererContract
 import org.http4k.contract.contract
-import org.http4k.contract.jsonschema.JsonSchema
 import org.http4k.contract.jsonschema.v3.KondorJsonSchemaCreator
 import org.http4k.contract.meta
 import org.http4k.contract.openapi.AddSimpleFieldToRootNode
 import org.http4k.contract.openapi.ApiInfo
 import org.http4k.contract.openapi.ApiRenderer
 import org.http4k.core.HttpMessage
-import org.http4k.core.Method
+import org.http4k.core.Method.GET
+import org.http4k.core.Method.POST
 import org.http4k.core.Request
 import org.http4k.core.Response
-import org.http4k.core.Status
+import org.http4k.core.Status.Companion.OK
 import org.http4k.core.Uri
 import org.http4k.format.KondorJson
 import org.http4k.format.autoBody
 import org.http4k.routing.bind
 import org.http4k.testing.Approver
 import org.junit.jupiter.api.Test
-
-private data class NestedObject(val aNullField: String?, val aNumberField: Int)
-private data class TopLevelObject(val nestedObject: NestedObject)
-
-private object JNestedObject : JAny<NestedObject>() {
-    private val aNullField by str(NestedObject::aNullField)
-    private val aNumberField by num(NestedObject::aNumberField)
-
-    override fun JsonNodeObject.deserializeOrThrow(): NestedObject =
-        NestedObject(
-            aNullField = +aNullField,
-            aNumberField = +aNumberField
-        )
-}
-
-private object JTopLevelObject : JAny<TopLevelObject>() {
-    private val nestedObject by obj(JNestedObject, TopLevelObject::nestedObject)
-
-    override fun JsonNodeObject.deserializeOrThrow(): TopLevelObject =
-        TopLevelObject(
-            nestedObject = +nestedObject
-        )
-}
-
-private val kondor = KondorJson {
-    register(TopLevelObject::class, JTopLevelObject)
-}
 
 class OpenApi3KondorTest : ContractRendererContract<JsonNode>(
     kondor,
@@ -63,7 +36,6 @@ class OpenApi3KondorTest : ContractRendererContract<JsonNode>(
         extensions = listOf(AddSimpleFieldToRootNode),
     )
 ) {
-
     @Test
     override fun `renders as expected`(approver: Approver) {
         /*
@@ -86,9 +58,7 @@ class OpenApi3KondorTest : ContractRendererContract<JsonNode>(
                 approver.assertApproved(httpMessage.body(scrubbedBody))
             }
 
-            override fun withNameSuffix(suffix: String): Approver {
-                TODO("Not yet implemented")
-            }
+            override fun withNameSuffix(suffix: String) = this
         }
 
         super.`renders as expected`(scrubbingApprover)
@@ -104,30 +74,46 @@ class OpenApi3KondorTest : ContractRendererContract<JsonNode>(
 
             routes += "/body_json_schema_data_class" meta {
                 receiving(topLevelObjectLens to example)
-            } bindContract Method.POST to { _ -> Response(Status.OK) }
+            } bindContract POST to { _ -> Response(OK) }
 
             routes += "/body_json_schema_data_class_with_definition_id" meta {
                 receiving(topLevelObjectLens to example, "OverriddenDefinitionId", "a_prefix_")
-            } bindContract Method.POST to { _ -> Response(Status.OK) }
+            } bindContract POST to { _ -> Response(OK) }
         }
 
-        approver.assertApproved(router(Request(Method.GET, "/basepath?the_api_key=somevalue")))
+        approver.assertApproved(router(Request(GET, "/basepath?the_api_key=somevalue")))
     }
 
     // this serves as an example of how the KondorJsonToJsonSchema can be used to render the OpenApi3 contract
     private class KondorOpenApi3Renderer(
         kondorJson: KondorJson,
-        private val delegate: OpenApi3ApiRenderer<JsonNode>,
-        refLocationPrefix: String = "components/schemas",
+        private val delegate: OpenApi3ApiRenderer<JsonNode>
     ) : ApiRenderer<Api<JsonNode>, JsonNode> by delegate {
-        private val jsonToJsonSchema = KondorJsonSchemaCreator(kondorJson, refLocationPrefix)
+        private val jsonToJsonSchema = KondorJsonSchemaCreator(kondorJson)
 
-        override fun toSchema(
-            obj: Any,
-            overrideDefinitionId: String?,
-            refModelNamePrefix: String?,
-        ): JsonSchema<JsonNode> {
-            return jsonToJsonSchema.toSchema(obj, overrideDefinitionId, refModelNamePrefix)
-        }
+        override fun toSchema(obj: Any, overrideDefinitionId: String?, refModelNamePrefix: String?) =
+            jsonToJsonSchema.toSchema(obj, overrideDefinitionId, refModelNamePrefix)
     }
+}
+
+private val kondor = KondorJson {
+    register(TopLevelObject::class, JTopLevelObject)
+}
+
+private data class TopLevelObject(val nestedObject: NestedObject)
+private data class NestedObject(val aNullField: String?, val aNumberField: Int)
+
+private object JTopLevelObject : JAny<TopLevelObject>() {
+    private val nestedObject by obj(JNestedObject, TopLevelObject::nestedObject)
+
+    override fun JsonNodeObject.deserializeOrThrow(): TopLevelObject =
+        TopLevelObject(nestedObject = +nestedObject)
+}
+
+private object JNestedObject : JAny<NestedObject>() {
+    private val aNullField by str(NestedObject::aNullField)
+    private val aNumberField by num(NestedObject::aNumberField)
+
+    override fun JsonNodeObject.deserializeOrThrow(): NestedObject =
+        NestedObject(aNullField = +aNullField, aNumberField = +aNumberField)
 }
